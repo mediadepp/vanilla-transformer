119a06ff2315be19641787c2368486553e164dc9
Media Depp <mediadepp@gmail.com>
2022-07-22 09:41:30 +0000

    Models and uitl packages are made. I've mentioned in the init files that I'm not the original author of the code. I've also added the .gitignore file.

---
+++ transformer/models/__init__.py
@@ -0,0 +1,5 @@
+"""
+This implementation is like the one implemented by @author : Hyunwoong
+You can find the original code at the following repository: 
+@homepage : https://github.com/gusdnd852
+"""
