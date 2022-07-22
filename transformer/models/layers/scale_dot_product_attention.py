b10f10f6cd5c9c80ecc0ea1bbac44e623fdee6cd
Media Depp <mediadepp@gmail.com>
2022-07-22 10:10:12 +0000

    The layer package is created. scale_dot_product_attention is added.

---
+++ transformer/models/layers/scale_dot_product_attention.py
@@ -0,0 +1,19 @@
+import math
+from torch import nn 
+
+
+class ScaleDotProductAttention(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.softmax = nn.Softmax(dim=-1)
+    
+    def forward(self, q, k, v, mask=None, e=1e-12):
+        batch_size, head, length, d_tensor = k.shape
+        k_t = k.transpose(-2, -3)
+        score = (q @ k_t) / math.sqrt(d_tensor)
+        if mask is not None: 
+            score = score.masked_fill(mask == 0, -e)
+
+        score = self.softmax(score) 
+        v = score @ v
+        return v, score
